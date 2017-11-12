025c8b9f1446b35825d8534438800a27fe75b026
Ekhlas Sonu <ekhlas.sonu@gmail.com>
2017-11-12 11:52:39 -0800

    Just checking if it works

--- src/IPOMCP.jl
+++ src/IPOMCP.jl
@@ -1,5 +1,18 @@
 module IPOMCP
 
 # package code goes here
+import Base: ==, +, *, -, <, >, copy, Random, hash, length, rand, convert
+importall POMDPs, MCVI
+import ParticleFilters: obs_weight
+using POMDPToolbox
+using DataFrames
+using PmapProgressMeter
+using ProgressMeter
+using MCTS
+using Parameters # for @with_kw
+using AutoHashEquals
+using StaticArrays
+using JLD
+
 
 end # module
