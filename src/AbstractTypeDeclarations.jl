41a4bdb20a70401df6347fd19c4e168dd5485dc3
Ekhlas Sonu <ekhlas.sonu@gmail.com>
2018-01-15 16:04:03 -0800

    Testing git upstream

--- src/AbstractTypeDeclarations.jl
+++ src/AbstractTypeDeclarations.jl
@@ -6,4 +6,5 @@ abstract type AbstractInteractiveState{S} end
 abstract type AbstractIPOMDP <: Frame end
 abstract type AbstractParticleInteractiveBelief{T} <: AbstractParticleBelief{T} end
 
+
 abstract type AbstractIPOMDPSolver end
